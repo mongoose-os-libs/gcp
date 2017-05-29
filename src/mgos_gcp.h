52e7f3a4f740e24b4c630d49195a2209d18e5a38
Dmitry Frank <mail@dmitryfrank.com>
2017-05-29 12:01:18 +0300

    Factor out GCP into separate lib
    PUBLISHED_FROM=3e7260a0651ef3ea6f60d71307d0e6235356b158

---
+++ src/mgos_gcp.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2014-2017 Cesanta Software Limited
+ * All rights reserved
+ */
+
+#ifndef CS_MOS_LIBS_GCP_SRC_MGOS_GCP_H_
+#define CS_MOS_LIBS_GCP_SRC_MGOS_GCP_H_
+
+#include "fw/src/mgos_features.h"
+#include "fw/src/mgos_init.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+enum mgos_init_result mgos_gcp_init(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* CS_MOS_LIBS_GCP_SRC_MGOS_GCP_H_ */
